d38752afe5e1fee0e778473567a28454d399470f
Armand Emamdjomeh <armand.emamdjomeh@latimes.com>
2016-05-18 11:36:46 -0700

    removing pycs and adding to gitignore

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -1,8 +1,10 @@
 from __future__ import absolute_import
+from .clean import clean
 from .copy_secrets import copy_secrets
 from .deploy import deploy
 
 __all__ = (
+    "clean",
     "copy_secrets",
     "deploy"
 )
--- fabfile/copy_secrets.py
+++ fabfile/copy_secrets.py
@@ -1,4 +1,5 @@
 from fabric.api import local, task, settings
 
+@task
 def copy_secrets():
     pass
