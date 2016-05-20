8642dceef7512a13da65c0ee66cb4d32097b0626
Armand Emamdjomeh <armand.emamdjomeh@latimes.com>
2016-05-20 12:16:48 -0700

    update to export URL

--- src/js/components/ChartExport.jsx
+++ src/js/components/ChartExport.jsx
@@ -264,7 +264,7 @@ var ChartExport = React.createClass({
 						{chartExportButtons}
 					</div>
 				<div className="instructions hidden" id="export-instructions">
-			        <p>That's it, you're done! You can find your chart in P2P under the slug<br/><span className='slug-label'><a target="_blank" href={'/get-p2p-admin-url/?slug=' + this.props.metadata.slug}>{this.props.metadata.slug}-chartbuilder</a></span>.</p>
+			        <p>That's it, you're done! You can find your chart in P2P under the slug<br/><span className='slug-label'><a target="_blank" href={'get-p2p-admin-url/?slug=' + this.props.metadata.slug}>{this.props.metadata.slug}-chartbuilder</a></span>.</p>
 		        </div>
 		        <p>ChartBuilder is an open source project created by <a href="https://github.com/Quartz/Chartbuilder/">Quartz</a>. Let us know if you <a href="mailto:yyartist@latimes.com?Subject=ChartBuilder bug report">find any bugs</a>.</p>
 			</div>
