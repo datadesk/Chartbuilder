f7789370214d137236b7ffc3a4e43782236dc5a8
Armand Emamdjomeh <armand.emamdjomeh@latimes.com>
2016-05-20 11:50:31 -0700

    updating copy_secrets

---
+++ fabfile/copy_secrets.py
@@ -0,0 +1,14 @@
+import os
+from fabric.api import task, put, env
+
+
+@task
+def copy_secrets():
+    """
+    Copies our settings file onto a remote server
+    """
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    secrets_dir = os.path.join(base_dir, 'app', 'secrets')
+    remote_dir = os.path.join(env.project_dir, 'app', 'secrets')
+
+    put(secrets_dir, remote_dir)
