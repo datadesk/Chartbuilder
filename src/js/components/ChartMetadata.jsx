33223ea384497d3a0634daba891b06579038188b
Armand Emamdjomeh <armand.emamdjomeh@latimes.com>
2016-06-27 17:10:02 -0700

    updating build

--- src/js/components/ChartMetadata.jsx
+++ src/js/components/ChartMetadata.jsx
@@ -155,7 +155,11 @@ var ChartMetadata = React.createClass({
 				</h2>
 				{textInputs}
 				{this.props.additionalComponents}
-
+				<ButtonGroup
+					buttons={chart_sizes}
+					onClick={this._handleMetadataUpdate.bind(null, "size")}
+					value={metadata.size}
+				/>
 			</div>
 		);
 	}
